57a61739f811d0f85d5a21acdfe50880b4c19986
sajal <sajalsahu8@gmail.com>
2023-07-21 03:44:56 +0400

    completely location widget

--- lib/widgets/locationinputwidget.dart
+++ lib/widgets/locationinputwidget.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 
 class LocationInput extends StatefulWidget {
@@ -12,6 +13,35 @@ class LocationInput extends StatefulWidget {
 class _LocationInputState extends State<LocationInput> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Column(
+      children: [
+        Container(),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            //one to get automatically the location
+            TextButton.icon(
+              icon: const Icon(
+                Icons.location_on,
+              ),
+              onPressed: () {},
+              label: const Text(
+                "Get Current Location",
+              ),
+            ),
+            //one to get the map
+            TextButton.icon(
+              icon: const Icon(
+                Icons.map,
+              ),
+              onPressed: () {},
+              label: const Text(
+                "Select Location Manually",
+              ),
+            ),
+          ],
+        ),
+      ],
+    );
   }
 }
