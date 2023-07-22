63e8124a22affde3e4bdbac8a1e4be7af6de8d3a
sajal <sajalsahu8@gmail.com>
2023-07-22 17:34:13 +0400

    creating map screen

---
+++ lib/screens/map.dart
@@ -0,0 +1,22 @@
+import 'package:favorite_places/models/place.dart';
+import 'package:flutter/gestures.dart';
+import 'package:flutter/material.dart';
+
+class MapScreen extends StatefulWidget {
+  const MapScreen({
+    super.key,
+    this.location = const PlaceLocation(
+        latitude: 37.42, longitude: -122.08, address: "Googleplex"),
+  });
+
+  final PlaceLocation location;
+  @override
+  _MapScreenState createState() => _MapScreenState();
+}
+
+class _MapScreenState extends State<MapScreen> {
+  @override
+  Widget build(context) {
+    return Text("");
+  }
+}
