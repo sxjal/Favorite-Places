4b335a468c7dec9710c2ee41d6d6fd1759721930
sajal <sajalsahu8@gmail.com>
2023-07-23 01:15:12 +0400

    getting filename

--- lib/providers/additemprovider.dart
+++ lib/providers/additemprovider.dart
@@ -1,14 +1,18 @@
 // ignore_for_file: file_names
 
 import "package:favorite_places/models/place.dart";
-
+import "package:path_provider/path_provider.dart" as syspath;
+import "package:path/path.dart" as path;
 import "package:flutter_riverpod/flutter_riverpod.dart";
 
 class AddItemNotifier extends StateNotifier<List<Place>> {
   AddItemNotifier() : super([]);
   //initialstate
 
-  void addplace(Place place) {
+  void addplace(Place place) async {
+    final appDir = await syspath.getApplicationDocumentsDirectory();
+    final filename = path.basename(place.image.path);
+    
     state = [...state, place];
   }
 
